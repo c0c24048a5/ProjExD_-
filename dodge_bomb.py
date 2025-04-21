6c080edd84473864bef0c818186ff1ff3b32abbc
Haruki-0423 <c0c24048a5@edu.teu.ac.jp>
2025-04-21 16:05:30 +0900

    練習３

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -18,7 +18,7 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")    
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
-    kk_rct.center = 300, 200
+    kk_rct.center = 100, 200
     clock = pg.time.Clock()
     tmr = 0
     DELTA = {"up":None, "down":None, "left":None, "right":None}
@@ -26,6 +26,20 @@ def main():
     B = 0
     C = 0
     D = 0
+    x = 0
+    y = 0
+    def cheak_bound(position):
+        size = int(str(position[3]))
+        if position[0] >= 1100 - size:
+            return "xout_left"
+        elif position[0] <= 0:
+            return "xout_right"
+        elif position[1] >= 650 - size:
+            return "yout_down" 
+        elif position[1] <= 0:
+            return "yout_up"
+        else:
+            return True
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -33,30 +47,40 @@ def main():
         screen.blit(bg_img, [0, 0]) 
 
         bb_move = [0, 0]
-        bb_move[0] += 5
-        bb_move[1] += 5
+        if cheak_bound(bb_rct) == "xout_left" or cheak_bound(bb_rct) == "xout_right":
+            x += 1
+        elif cheak_bound(bb_rct) == "yout_down" or cheak_bound(bb_rct) == "yout_up":
+            y += 1
+        bb_move[0] += 5 * ((-1)**x)
+        bb_move[1] += 5 * ((-1)**y)
+
+    
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         if key_lst[pg.K_UP]:
-            sum_mv[1] -= 5
-            A += int(sum_mv[1])
-            DELTA["up"] = A
+            if cheak_bound(kk_rct) != "yout_up":    
+                sum_mv[1] -= 5
+                A += int(sum_mv[1])
+                DELTA["up"] = A
 
         if key_lst[pg.K_DOWN]:
-            sum_mv[1] += 5
-            B += int(sum_mv[1])
-            DELTA["down"] = B
+            if cheak_bound(kk_rct) != "yout_down":
+                sum_mv[1] += 5
+                B += int(sum_mv[1])
+                DELTA["down"] = B
             
         if key_lst[pg.K_LEFT]:
-            sum_mv[0] -= 5
-            C += int(sum_mv[0])
-            DELTA["left"] = C
+            if cheak_bound(kk_rct) != "xout_right":
+                sum_mv[0] -= 5
+                C += int(sum_mv[0])
+                DELTA["left"] = C
 
         if key_lst[pg.K_RIGHT]:
-            sum_mv[0] += 5
-            D += int(sum_mv[1])
-            DELTA["right"] = D
+            if cheak_bound(kk_rct) != "xout_left":
+                sum_mv[0] += 5
+                D += int(sum_mv[1])
+                DELTA["right"] = D
              
         kk_rct.move_ip(sum_mv)
         bb_rct.move_ip(bb_move)
@@ -65,7 +89,7 @@ def main():
         pg.display.update()
         tmr += 1
         clock.tick(50)
-        print(DELTA)
+        print(kk_rct)
 
 
 if __name__ == "__main__":
